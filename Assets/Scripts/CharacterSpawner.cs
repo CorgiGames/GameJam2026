3f54f8237557d84d8ceaa83adbd1af5ebad41cf4
HawkingBey <88308885+HawkingBey@users.noreply.github.com>
2026-02-28 18:14:29 +0200

    Fixed bugs

--- Assets/Scripts/CharacterSpawner.cs
+++ Assets/Scripts/CharacterSpawner.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.Events;
 
 public class CharacterSpawner : MonoBehaviour
 {
@@ -11,12 +12,20 @@ public class CharacterSpawner : MonoBehaviour
     [SerializeField] private float timeBetweenWaves = 5f;
     [SerializeField] private float difficultyScalingFactor = 0.75f;
 
+    [Header("Events")]
+    public static UnityEvent onCharacterDestroy = new UnityEvent();
+
     private int currentWave = 1;
     private float timeSinceLastSpawn;
     private int charactersAlive;
     private int charactersLeftToSpawn;
     private bool isSpawning = false;
 
+
+    private void Awake()
+    {
+        onCharacterDestroy.AddListener(CharacterDestroyed);
+    }
     private void Start()
     {
         StartWave();
@@ -28,16 +37,29 @@ public class CharacterSpawner : MonoBehaviour
             
         }
         timeSinceLastSpawn += Time.deltaTime;
-        if(timeSinceLastSpawn >= (1f / characterPerSecond))
+        if(timeSinceLastSpawn >= (1f / characterPerSecond) && charactersLeftToSpawn > 0)
         {
-            Debug.Log("Spawn");
+            SpawnCharacter();
+            charactersLeftToSpawn --;
+            charactersAlive ++;
+            timeSinceLastSpawn = 0f;
         }
     }
 
+    private void CharacterDestroyed() 
+    {
+        charactersAlive --;
+    }
     private void StartWave()
     {
         isSpawning = true;
         charactersLeftToSpawn = baseCharacters;
+    }
+
+    private void SpawnCharacter()
+    {
+        GameObject prefabToSpawn = characterPrefabs[0];
+        Instantiate(prefabToSpawn, LevelManager.main.startPoint.position, Quaternion.identity);
     }
     private int CharacterPerWave()
     {
