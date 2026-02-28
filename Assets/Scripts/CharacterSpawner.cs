661b174921ec8bbeb54b5dafd49989f902667136
HawkingBey <88308885+HawkingBey@users.noreply.github.com>
2026-02-28 17:42:29 +0200

    Added more sounds

--- Assets/Scripts/CharacterSpawner.cs
+++ Assets/Scripts/CharacterSpawner.cs
@@ -9,4 +9,38 @@ public class CharacterSpawner : MonoBehaviour
     [SerializeField] private int baseCharacters = 8;
     [SerializeField] private float characterPerSecond = 0.5f;
     [SerializeField] private float timeBetweenWaves = 5f;
+    [SerializeField] private float difficultyScalingFactor = 0.75f;
+
+    private int currentWave = 1;
+    private float timeSinceLastSpawn;
+    private int charactersAlive;
+    private int charactersLeftToSpawn;
+    private bool isSpawning = false;
+
+    private void Start()
+    {
+        StartWave();
+    }
+    private void Update()
+    {
+        if (!isSpawning) return;
+        {
+            
+        }
+        timeSinceLastSpawn += Time.deltaTime;
+        if(timeSinceLastSpawn >= (1f / characterPerSecond))
+        {
+            Debug.Log("Spawn");
+        }
+    }
+
+    private void StartWave()
+    {
+        isSpawning = true;
+        charactersLeftToSpawn = baseCharacters;
+    }
+    private int CharacterPerWave()
+    {
+        return Mathf.RoundToInt(baseCharacters * Mathf.Pow(currentWave, difficultyScalingFactor));
+    }
 }
