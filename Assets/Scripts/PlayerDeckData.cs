c6e22483873853ac4d2d5bc5cd3b3f3eec64dee9
arda-sahin <a.sahin0002@gmail.com>
2026-03-01 08:10:24 +0200

    commit

--- Assets/Scripts/PlayerDeckData.cs
+++ Assets/Scripts/PlayerDeckData.cs
@@ -5,9 +5,16 @@ using UnityEngine;
 public class PlayerDeckData : ScriptableObject
 {
     public List<CardData> savedDeck = new List<CardData>();
+    public int currentRound = 1;
 
     public void ClearDeck()
     {
         savedDeck.Clear();
+    }
+
+    public void ResetGameData()
+    {
+        savedDeck.Clear();
+        currentRound = 1;
     }
 }
